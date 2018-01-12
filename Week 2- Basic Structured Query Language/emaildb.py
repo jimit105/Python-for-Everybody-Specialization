c3b2b2b03c37ef23c47a049d20fbedda1e124963
Jimit Dholakia <jimitd101@gmail.com>
2018-01-12 23:04:18 +0530

    Couting Email in a Database

---
+++ Week 2- Basic Structured Query Language/emaildb.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 12 22:14:50 2018
+
+@author: Jimit
+"""
+
+# Counting Email in a Database
+
+import sqlite3
+
+conn = sqlite3.connect('emaildb.sqlite')
+cur = conn.cursor()
+
+cur.execute('DROP TABLE IF EXISTS Counts')
+
+cur.execute('''
+            CREATE TABLE Counts(email TEXT, count INTEGER)''')
+
+fname = input('Enter file name: ')
+if(len(fname) < 1): fname = 'mbox-short.txt'
+
+fh = open(fname)
+
+for line in fh:
+    if not line.startswith('From: '): continue
+    pieces = line.split()
+    email = pieces[1]
+    cur.execute('SELECT count FROM Counts WHERE email = ?', (email,))
+    # (email,) is a one-element tuple
+    # (email) won't turn 'email' into a tuple
+    # so we need to add a comma after 'email'
+    
+    
+    row = cur.fetchone()
+    
+    if row is None:
+        cur.execute('''INSERT INTO Counts(email, count)
+        VALUES (?, 1)''', (email,))
+    else:
+        cur.execute('UPDATE Counts SET count = count + 1 WHERE email = ?', (email,))
+        
+conn.commit()
+    
+sqlstr = 'SELECT email, count FROM Counts ORDER BY count DESC LIMIT 10'
+
+for row in cur.execute(sqlstr):
+    print(str(row[0]), row[1])
+
+cur.close()
