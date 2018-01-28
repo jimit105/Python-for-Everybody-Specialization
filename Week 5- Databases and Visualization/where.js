1371837e5691d1ea118719a41c58095aa2c19524
Jimit Dholakia <jimitd101@gmail.com>
2018-01-29 00:24:29 +0530

    Databases and Visualization
    Week 5

---
+++ Week 5- Databases and Visualization/where.js
@@ -0,0 +1,6 @@
+myData = [
+[50.0676785,19.9070824, 'Dormitory no. 14 "Kapitol" of AGH University of Science and Technology in Cracov, Poland, Witolda Budryka 2, 30-072 Kraków, Poland'],
+[52.2296756,21.0122287, 'Warsaw, Poland'],
+[28.3580163,75.5887989, 'BITS, Pilani, Rajasthan 333031, India'],
+[43.1294077,-77.5479304, 'Babcock Dr, Rochester, NY 14610, USA']
+];
